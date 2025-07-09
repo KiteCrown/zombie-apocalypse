c53daae9cccbc31564683eec86bb695166b7fbb4
KiteCrown <162701747+KiteCrown@users.noreply.github.com>
2025-07-09 21:55:08 +0800

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,26 @@
+ 
+
+
+> Open this page at [https://kitecrown.github.io/zombie-apocalypse/](https://kitecrown.github.io/zombie-apocalypse/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/kitecrown/zombie-apocalypse** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/kitecrown/zombie-apocalypse** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,76 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (saasdokodasp == 0) {
+        myBall2 = carnival.createProjectileBallFromSprite(img`
+            5 5 5 5 
+            5 5 5 5 
+            5 5 5 5 
+            5 5 5 5 
+            `, mySprite, SpriteKind.Projectile)
+        saasdokodasp = 1
+        timer.after(d, function () {
+            saasdokodasp = 0
+        })
+    }
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite, effects.fire, 500)
+    statusbar.value += 1
+    info.changeScoreBy(1)
+})
+statusbars.onStatusReached(StatusBarKind.Health, statusbars.StatusComparison.EQ, statusbars.ComparisonType.Percentage, 100, function (status) {
+    statusbar.max += 5
+    statusbar.value = 0
+    d += -50
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    game.setGameOverMessage(true, "GAME OVER!")
+    game.setGameOverEffect(true, effects.melt)
+    game.setGameOverPlayable(true, music.melodyPlayable(music.wawawawaa), false)
+    game.gameOver(true)
+})
+let mySprite2: Sprite = null
+let myBall2: Ball = null
+let mySprite: Ball = null
+let statusbar: StatusBarSprite = null
+let d = 0
+let saasdokodasp = 0
+saasdokodasp = 0
+d = 700
+let spanwwq = 1500
+let sadsda = 10
+tiles.loadMap(tiles.createSmallMap(tilemap`level2`))
+statusbar = statusbars.create(50, 4, StatusBarKind.Energy)
+statusbar.value = 0
+statusbar.max = 3
+statusbar.positionDirection(CollisionDirection.Bottom)
+statusbar.setBarBorder(1, 5)
+mySprite = carnival.create(img`
+    8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 
+    `, SpriteKind.Player, 80, 60)
+mySprite.controlBallWithArrowKeys()
+mySprite.setTraceMulti(carnival.Tracers.Part)
+game.onUpdateInterval(spanwwq, function () {
+    mySprite2 = sprites.create(img`
+        . . . . . . . . 
+        . . 2 2 2 2 . . 
+        . . 2 6 2 2 . . 
+        . . 2 6 6 2 . . 
+        . . 6 6 6 2 . . 
+        . . 6 6 6 6 . . 
+        . . 6 6 6 6 . . 
+        . . 6 6 6 6 . . 
+        `, SpriteKind.Enemy)
+    mySprite2.follow(mySprite, sadsda)
+    tiles.placeOnRandomTile(mySprite2, assets.tile`myTile0`)
+})
+game.onUpdateInterval(20000, function () {
+    sadsda += 5
+    spanwwq += -30
+})
